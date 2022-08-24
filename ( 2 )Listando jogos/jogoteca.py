7288fc5726440da54ef402860aeb624cc3a8f424
gssouza2051 <gssouza2051@gmail.com>
2022-08-23 23:44:11 -0300

    first commit

---
+++ ( 2 )Listando jogos/jogoteca.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template
+
+class jogo:
+    def __init__(self,nome,categoria, console):
+        self.nome=nome
+        self.categoria=categoria
+        self.console=console
+
+app = Flask(__name__)
+
+@app.route('/inicio')
+def ola():
+    jogo1 = jogo('Tetris','Puzzle','Atari')
+    jogo2 = jogo('God of War','Rack n Slash', 'PS2')
+    jogo3 = jogo('Mortal Kombat','Luta','PS3')
+    lista = [jogo1,jogo2,jogo3]
+    return render_template('lista.html',titulo="Jogos", jogos=lista);
+
+app.run()
+
